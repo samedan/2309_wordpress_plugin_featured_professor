55e52d4a8b9f906f7dab382a2b40ee8b5feeadb8
Daniel Popescu <same.dan@gmail.com>
2023-09-11 10:46:26 +0200

    Saving selected options in the DBB

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
-import "./index.scss"
+import "./index.scss";
 
 wp.blocks.registerBlockType("ourplugin/featured-professor", {
   title: "Professor Callout",
-  description: "Include a short description and link to a professor of your choice",
+  description:
+    "Include a short description and link to a professor of your choice",
   icon: "welcome-learn-more",
   category: "common",
+  attributes: {
+    profId: { type: "string" },
+  },
   edit: EditComponent,
   save: function () {
-    return null
-  }
-})
+    return null;
+  },
+});
 
-function EditComponent() {
+function EditComponent(props) {
   return (
     <div className="featured-professor-wrapper">
       <div className="professor-select-container">
-        We will have a select dropdown form element here.
+        <select
+          onChange={(e) => props.setAttributes({ profId: e.target.value })}
+        >
+          <option value="">Select a professor</option>
+          <option value="1" selected={props.attributes.profId == 1}>
+            1
+          </option>
+          <option value="2" selected={props.attributes.profId == 2}>
+            2
+          </option>
+          <option value="3" selected={props.attributes.profId == 3}>
+            3
+          </option>
+        </select>
       </div>
-      <div>
-        The HTML preview of the selected professor will appear here.
-      </div>
+      <div>The HTML preview of the selected professor will appear here.</div>
     </div>
-  )
+  );
 }
